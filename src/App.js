99fc51f7f4d5a0213dc26cec4f51f7c69e6a76b6
dega_k <degakon@gmail.com>
2019-02-06 20:06:40 +0100

    working state

--- src/App.js
+++ src/App.js
@@ -2,49 +2,19 @@ import React, { Component } from "react";
 import "./App.css";
 import Data from "./Data.json";
 import DropDown from "./DropDown";
-import DropDownListItem from "./DropDownListItem";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: Data,
-      showDropDown: false
+      data: Data
     };
-  }
-
-  toggleDropDown() {
-    this.setState(state => {
-      state.showDropDown = !state.showDropDown;
-      return state;
-    });
   }
 
   render() {
     return (
       <div className="App">
-        <DropDown />
-        <div className="dropdown-container">
-          <div
-            className="dropdown-header cursor-pointer"
-            onClick={this.toggleDropDown.bind(this)}
-          >
-            {this.state.showDropDown ? (
-              <i className="dropdown-header-icon fas fa-angle-down" />
-            ) : (
-              <i className="dropdown-header-icon fas fa-angle-up" />
-            )}
-            <div className="dropdown-header-title">Menu</div>
-          </div>
-
-          {this.state.showDropDown && (
-            <ul className="dropdown-list">
-              {Object.keys(this.state.data).map(id => {
-                return <DropDownListItem key={id} data={this.state.data[id]} />;
-              })}
-            </ul>
-          )}
-        </div>
+        <DropDown data={this.state.data} />
       </div>
     );
   }
--- src/DropDown.js
+++ src/DropDown.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
+import DropDownListItem from "./DropDownListItem";
 
-function DropDownListItem(props) {
-  return (
-    <li className="dropdown-list-item cursor-pointer">{props.data.title}</li>
-  );
+class DropDown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropDown: false
+    };
+  }
+
+  toggleDropDown() {
+    this.setState(state => {
+      state.showDropDown = !state.showDropDown;
+      return state;
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div
+          className="dropdown-header cursor-pointer"
+          onClick={this.toggleDropDown.bind(this)}
+        >
+          {this.state.showDropDown ? (
+            <i className="dropdown-header-icon fas fa-angle-down" />
+          ) : (
+            <i className="dropdown-header-icon fas fa-angle-up" />
+          )}
+          <div className="dropdown-header-title">Menu</div>
+        </div>
+
+        {this.state.showDropDown && (
+          <ul className="dropdown-list">
+            {Object.keys(this.props.data).map(id => {
+              return <DropDownListItem key={id} data={this.props.data[id]} />;
+            })}
+          </ul>
+        )}
+      </Fragment>
+    );
+  }
 }
 
-export default DropDownListItem;
+export default DropDown;
