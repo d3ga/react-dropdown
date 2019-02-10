2aebf699c3c9eac0efdd3fa41be2393f1d52f10e
dega_k <degakon@gmail.com>
2019-02-10 12:35:29 +0100

    Cleaning up

--- src/DropDownListItem.js
+++ src/DropDownListItem.js
@@ -1,23 +1,6 @@
-// import React from "react";
-
-// function DropDownListItem(props) {
-//   return (
-//     <li className="drop-down-list-item cursor-pointer">{props.data.title}</li>
-//   );
-// }
-
-// export default DropDownListItem;
-
 import React, { Component } from "react";
 
 class DropDownListItem extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.state = {
-  //   //   selected: []
-  //   // };
-  // }
-
   render() {
     let option = this.props.data.title;
 
